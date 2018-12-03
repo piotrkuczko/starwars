59e39d0d521dc9d3322b10acca6544f913e9d04b
Reina0111 <kusmierczyk.kasia1101@gmail.com>
2018-12-03 13:58:44 +0100

    rebelfleet + imperialfleet - gotowe, battle pozostał atak

--- battle.h
+++ battle.h
@@ -5,60 +5,64 @@
 #include <limits>
 #include <tuple>
 #include <cassert>
-#include <any>
 #include <cmath>
-#include <set>
-#include <vector>
+#include <array>
+#include <algorithm>
 #include "rebelfleet.h"
 #include "imperialfleet.h"
 
 template <typename T, T t0, T t1, typename ... S> class SpaceBattle {
-private:
-    std::vector<std::any> rebels;
-    std::vector<std::any> imperials;
+    static constexpr size_t countTimes(){
+        size_t i = 1;
+        while(i*i <= t1)
+            i++;
+        return i;
+    }
+
+    static constexpr std::array<T, countTimes()> set_attack_times(){
+        std::array<T, countTimes()> attackTimes;
+        size_t index = 0;
+        for(size_t i = 0; i*i<=t1; i++){
+            attackTimes[index] = i*i;
+            index++;
+        }
+        return attackTimes;
+    }
+
+    std::array<T, countTimes()> attackTimes {set_attack_times()};
+
+    std::tuple<S...> starShips;
     size_t rebelsFleet;
     size_t imperialsFleet;
     T currentTime;
-    std::set<T> attacks;
-
-    constexpr void set_attack_time(){
-        size_t time = 0;
-        for(size_t i = t0; i <= t1; i++){
-            time = sqrt(i);
-            if(time*time == i){
-                attacks.insert(i);
-            }
-        }
-    }
-
-    template <typename Imp, typename Reb>
-    void imperial_attacks(Imp imperial, Reb rebel){
-/*
-        if(imperial.getShield()>0 && rebel.getShield()>0){
-            attack(imperial, rebel);
-        }
-        if(imperial.getShield() == 0)
-            imperialsFleet--;
-        if(rebel.getShield() == 0)
-            rebelsFleet--;
- */
-    }
 
     template <typename Q>
-    void add_ships(Q ship){
-        if(ship.isRebel())
-            rebels.push_back(ship);
-        else
-            imperials.push_back(ship);
+    void countRebels(Q ship){
+        if(ship.getShield() > 0) {
+            if (ship.isRebel())
+                rebelsFleet++;
+        }
     }
 
     template <typename Q, typename ... R>
-    void add_ships(Q ship, R ... ships){
-        if(ship.isRebel())
-            rebels.push_back(ship);
-        else
-            imperials.push_back(ship);
-        add_ships(ships...);
+    void countRebels(Q ship, R... ships){
+        if(ship.getShield() > 0) {
+            if (ship.isRebel())
+                rebelsFleet++;
+        }
+        countRebels(ships...);
+    }
+
+
+
+    void imperialsAttack(){
+        /* dla każdego statku Imperium
+  dla każdego statku Rebelii
+    jeśli oba statki nie nie zostały jeszcze zniszczone,
+      statek Imperium atakuje statek Rebelii. */
+
+
+
     }
 
     void battle_result(){
@@ -73,18 +77,18 @@ private:
     }
 
 public:
-    explicit SpaceBattle(S... ships) {
+    explicit SpaceBattle(S... ships) : starShips(std::tuple<S...>(ships...)) {
         static_assert(t0 <= std::numeric_limits<T>::max() &&
                       t0 >= 0, "Starting time is not in range of given type");
         static_assert(t1 <= std::numeric_limits<T>::max() &&
                       t1 >= 0, "Ending time is not in range of given type");
         static_assert(t0 <= t1, "Ending must take place after the start of battle");
 
-        set_attack_time();
         currentTime = t0;
-        add_ships(ships...);
-        rebelsFleet = rebels.size();
-        imperialsFleet = imperials.size();
+        rebelsFleet = 0;
+        countRebels(ships...);
+        imperialsFleet = 0;
+        imperialsFleet = std::tuple_size<decltype(starShips)>::value - rebelsFleet;
 
     }
 
@@ -97,18 +101,14 @@ public:
     }
 
     void tick(T timestep){
-        if(attacks.find(currentTime) != attacks.end()) {
-            if (countRebelFleet()> 0 && countImperialFleet() > 0) {
-                for(auto Imperial : imperials){
-                    for(auto Rebel : rebels){
-                        imperial_attacks(Imperial, Rebel);
-                    }
-                }
+        if(std::find(attackTimes.begin(), attackTimes.end(), currentTime) != attackTimes.end()) {
+            if (countRebelFleet() > 0 && countImperialFleet() > 0) {
+                imperialsAttack();
             } else {
                 battle_result();
             }
         }
-        currentTime+=timestep;
+        currentTime += timestep;
         if(currentTime > t1) currentTime = currentTime-t1;
     }
 };
